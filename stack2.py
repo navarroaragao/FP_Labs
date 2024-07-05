c9080a149ee00bd6c792c0283d87230640c58501
Guilherme Ramos <guilherme.ramos@tecnico.ulisboa.pt>
2024-07-05 19:36:35 +0100

    Correccao de typos e um novo exercicio 1 no LAB10 que envolve fazer operacoes de alto nivel e verificar que funcionam independentemente da implementacao do TAD, duas implementacoes disponiveis para testar.

--- stack2.py
+++ stack2.py
@@ -1,40 +1,40 @@
 def new():
-    return ''
+    return 12
     
 def push(x, s):
-    if isinstance(x, int) and x >= 0 and isinstance(s, str):
-        return s+'12'+(''.join(str(ord(c)) for c in str(x)))
+    if isinstance(x, int) and x >= 0 and isinstance(s, int):
+        return int(str(s)+'12'+(''.join(str(ord(c)) for c in str(x))))
     else:
-        print('Argumento inválido (push).')
+        raise ValueError('Argumento inválido (push).')
         
 def pop(s):
-    if isinstance(s, str) and len(s)>1:
-        r = s[:]
+    if isinstance(s, int) and s > 0:
+        r = str(s)
         while(r[-2:]!='12'):
             r = r[:-2]
-        return r[:-2]
+        return int(r[:-2])
     else:
-        print('Argumento inválido (pop).')
+        raise ValueError('Argumento inválido (pop).')
 
 def top(s):
-    if isinstance(s, str) and len(s) > 1:
-        aux = s[:]
+    if isinstance(s, int) and s > 0:
+        aux = str(s)
         r = 0
         while(aux[-2:]!='12'):
             r = r*10+int(chr(int(aux[-2:])))
             aux = aux[:-2]
         return r   
     else:
-        print('Argumento inválido (top).')
+        raise ValueError('Argumento inválido (top).')
         
 def is_empty(s):
-    if isinstance(s, str):
-        return s == '' 
+    if isinstance(s, int):
+        return s == 12 
     else:
-        print('Argumento inválido (is_empty).')
+        raise ValueError('Argumento inválido (is_empty).')
         
 def clone(s):
-    if isinstance(s, str):
-        return s[:]
+    if isinstance(s, int):
+        return int(str(s)[:])
     else:
-        print('Argumento inválido (is_empty).')
+        raise ValueError('Argumento inválido (is_empty).')
