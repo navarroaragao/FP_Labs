ecee448cc862b2437e6914d50df3ad2dc2974e67
Guilherme Ramos <guilherme.ramos@tecnico.ulisboa.pt>
2024-07-08 11:36:40 +0100

    Correcoes em todos os notebooks e novos exercicios

--- stack.py
+++ stack.py
@@ -5,28 +5,28 @@ def push(x, s):
     if isinstance(x, int) and x >= 0 and isinstance(s, list):
         return s+[x]
     else:
-        print('Argumento inválido (push).')
+        raise ValueError('Argumento inválido (push).')
         
 def pop(s):
     if isinstance(s, list) and len(s)>0:
         return s[:-1]
     else:
-        print('Argumento inválido (pop).')
+        raise ValueError('Argumento inválido (pop).')
 
 def top(s):
     if isinstance(s, list) and len(s) > 0:
         return s[-1]  
     else:
-        print('Argumento inválido (top).')
+        raise ValueError('Argumento inválido (top).')
         
 def is_empty(s):
     if isinstance(s, list):
         return s == [] 
     else:
-        print('Argumento inválido (is_empty).')
+        raise ValueError('Argumento inválido (is_empty).')
         
 def clone(s):
     if isinstance(s, list):
         return s[:]
     else:
-        print('Argumento inválido (is_empty).')
+        raise ValueError('Argumento inválido (is_empty).')
